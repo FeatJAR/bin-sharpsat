be1821fb87fd0ec3c9b3376e706f040e8993697e
Elias Kuiter <info@elias-kuiter.de>
2022-08-13 11:42:26 +0200

    Formatting

--- src/main/java/de/featjar/bin/sharpsat/SharpSatBinary.java
+++ src/main/java/de/featjar/bin/sharpsat/SharpSatBinary.java
@@ -22,27 +22,21 @@ package de.featjar.bin.sharpsat;
 
 import de.featjar.util.bin.Binary;
 import de.featjar.util.bin.OperatingSystem;
-
 import java.nio.file.Path;
 import java.util.Set;
 
 public class SharpSatBinary extends Binary {
-    public SharpSatBinary() {
-    }
+    public SharpSatBinary() {}
 
     @Override
     public Path getPath() {
-        if (OperatingSystem.IS_WINDOWS)
-            return BINARY_DIRECTORY.resolve("sharpSAT.exe");
-        else
-            return BINARY_DIRECTORY.resolve("sharpSAT");
+        if (OperatingSystem.IS_WINDOWS) return BINARY_DIRECTORY.resolve("sharpSAT.exe");
+        else return BINARY_DIRECTORY.resolve("sharpSAT");
     }
 
     @Override
     public Set<String> getResourceNames() {
-        if (OperatingSystem.IS_WINDOWS)
-            return Set.of("sharpSAT.exe", "gmp-10.dll");
-        else
-            return Set.of("sharpSAT");
+        if (OperatingSystem.IS_WINDOWS) return Set.of("sharpSAT.exe", "gmp-10.dll");
+        else return Set.of("sharpSAT");
     }
 }
